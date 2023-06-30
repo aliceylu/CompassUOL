9360d042891f75042ddf6f9b6576520127b8549b
Alice Y. Lu <123412887+aliceylu@users.noreply.github.com>
2023-06-30 15:59:16 -0300

    Update upload_files.py

--- Sprint_7/Desafio_ETL/upload_files.py
+++ Sprint_7/Desafio_ETL/upload_files.py
@@ -7,9 +7,9 @@ from datetime import datetime
 
 s3 = boto3.client(
     's3',
-    aws_access_key_id="ASIA2CPG5L5WWK5QK6MB",
-    aws_secret_access_key="fOfKb3qiCljC5kDz6NEReTk+qzL5ak8NfmhPnHOw",
-    aws_session_token="IQoJb3JpZ2luX2VjEKH//////////wEaCXVzLWVhc3QtMSJGMEQCIEH9CCjaUXbJ1/el6/LZcXK3z/il4WWpKvehpDJ9uQR6AiAH0dh+4IgbjE/hMvPVa/zen2KpQG1HD0EIVfK3DeJGzCqeAwiq//////////8BEAAaDDY5MjUxMDg3NTUwMSIMCRMBt1zy8vMW8frmKvIC3DqLyh1FXWMZsXHc/FOoGYF5PqfeEAVH4OAyVAZTvAAZW86EpqKaPdRhSIxgHTZ8Emy8MZIFjZraO8mdu+vNA/w3DopYc+MzRuDZFS19n25obRMtyDhlTqvbvNm4vCkFuxgF9AmjdnxZd7UYB/jPwM9wBaeyNR3/OVmlSVn8gZUb9mAnv5RQoDKTQ9pyCxs8KgFyKSpOafgwGadYMMtf+miCN1I8fqegNz+Z0SduT73IfubNCeAKWimx7d1o8dDG3/A07yzFthr5KGcfSfHGfvEad35C+TtpQ/AZUXw223qLMojdtsipVDkxzazbafMVutaT3RyGQG3zDMHtlqtNn1FxcIHoI0SXVgocIjTJpNsB4w8kD5axKYXy9ZxpEy8ScYZpLZmABBB0heUNrwnNf8+NfPpSnJ6CbahX/and6/0Z6IyaXJPT0ZAorsraRlObcWLOZC9nI5uf08EmsaZ0aB2xWHTw1CXTESUMxR0L12iIETCU1b+iBjqnAXm/EIqY9ZwfLXiOxbECLgSgvkoQC1kOYpJ0tQDeaHugdPeM88M02DLd17KQIvwnD0D/GCHt2laEINlTS7bnsnh2JJWdprLgIPB9DHpO1s3oIcMLa1xljoY1bjY/ekt/xsMUwcXXWwUC8BHfXe8aLsZXQ2BlmYQzhdkxmoLmtCz3SziAPxvysudmW06svcDd1+rOCp9DEZskpCkApO+whAYZJAV1Ohvd")
+    aws_access_key_id="",
+    aws_secret_access_key="f",
+    aws_session_token="")
 
 path1 = os.getcwd()
 csv_files = glob(os.path.join(path1, "*.csv"))
